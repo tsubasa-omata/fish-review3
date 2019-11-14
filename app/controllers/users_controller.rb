2c8a5359875d092b6b2daa5604e4e3c3623da0bb
小俣翼 <runrunrun108@gmail.com>
2019-11-14 22:34:53 +0900

    userコントローラー、パーシャルの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 
-def new   
-  @user = User.new
-end
+  def new
+    @user = User.new
+  end
 
-def create
+  def create
     @user = User.new(user_params)
     if @user.save                                       #ここでbefore_actionのcreate_activation_digestが実行される。
-       @user.send_activation_email                      #このsend_activation_emailメソッドの中のaccount_activationはuser_mailerのメソッド？それともuser_mailer_previewのメソッド？
-        flash[:info] = "Please check your email to activate your account."
+      # @user.send_activation_email                      #このsend_activation_emailメソッドの中のaccount_activationはuser_mailerのメソッド？それともuser_mailer_previewのメソッド？
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
-     render "new"
-    end
+      render "new"
   end
+end
 
 
 private
