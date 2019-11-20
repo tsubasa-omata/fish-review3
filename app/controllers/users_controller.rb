83089bb21799ee0311c55cfad4112dd689ffcce6
小俣翼 <runrunrun108@gmail.com>
2019-11-20 14:02:25 +0900

    新規投稿機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,13 +51,6 @@ private
         )
     end
 
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "ログインして下さい"
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
