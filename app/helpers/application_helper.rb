ad45e2683a49d2e3e1193baa920f3e5f546f9199
小俣翼 <[runrunrun108@gmail.com]>
2019-12-26 18:27:43 +0900

    タイトル表示編集、text-overflowを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
   include UsersHelper
   
+  def full_title(page_title = '')
+    base_title = "Fish Review"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
