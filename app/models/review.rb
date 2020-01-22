a3ab5c9a426bc7aa8294c5de6a459e16b1aedcd3
Matsushin <matsu1983@gmail.com>
2020-01-22 21:10:11 +0900

    不要なバリデーション削除等リファクタ

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,22 +4,19 @@ class Review < ApplicationRecord
   has_one_attached :avatar
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+
   default_scope -> { order(created_at: :desc) }
   with_options presence: true do
     validates :taste_level
     validates :season
     validates :place
     validates :fish_size
-    validates :user_id
-    validates :fish_id
     validates :recipe
   end
 
-  validates :taste_level,  numericality: { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1}
+  validates :taste_level,  numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
   validates :taste_imp,    length:       { maximum: 200 }
   validates :place_detail, length:       { maximum: 10 }
   validates :recipe,       length:       { maximum: 10 }
-  validates :season,       numericality: { :less_than_or_equal_to => 12, :greater_than_or_equal_to => 1}
-
+  validates :season,       numericality: { less_than_or_equal_to: 12, greater_than_or_equal_to: 1}
 end
