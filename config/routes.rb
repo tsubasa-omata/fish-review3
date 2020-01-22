097c2eccb9921eb67afbe8609d8e3b4bd86bdaac
Matsushin <matsu1983@gmail.com>
2020-01-22 20:47:58 +0900

    ルーティングをリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -3,35 +3,29 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get 'static_pages/about' => 'static_pages#about'
-  resources :users
+  resources :users do
+    resources :relationships, only: [:create, :destroy]
+    member do
+      get :following, :followers
+    end
+  end
   #resources :likes, only: [:index]
   get 'likes/index' => 'likes#index'
-  resources :reviews do
-    resources :likes, only: [:create, :destroy]
-  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :relationships, only: [:create, :destroy]
-  resources :fishes, only: [:index, :show]
-  resources :reviews do
-    resources :comments, only: [:create, :destroy]
+  resources :fishes, only: [:index, :show] do
+    resources :reviews do
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
+    end
   end
   
   namespace :admin do
-    get    'users/edit' => 'users#edit'
-    delete 'users/:id' => 'users#destroy'
+    resources :users, only: [:edit, :destroy]
   end
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
- 
-  #get 'scrip/sc' => 'scrip#sc'
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-
 end
 
