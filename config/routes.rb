15cab883413ecccd4f24dd136e1e1f638bc810be
小俣翼 <[runrunrun108@gmail.com]>
2020-01-23 16:45:31 +0900

    routesリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-
-  
   root 'static_pages#home'
   get 'static_pages/about' => 'static_pages#about'
-  resources :users
-  #resources :likes, only: [:index]
   get 'likes/index' => 'likes#index'
-  resources :reviews do
-    resources :likes, only: [:create, :destroy]
-  end
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :relationships, only: [:create, :destroy]
   resources :fishes, only: [:index, :show]
+  
+  resources :reviews do
+    resources :likes, only: [:create, :destroy]
+  end
+  
   resources :reviews do
     resources :comments, only: [:create, :destroy]
   end
@@ -21,17 +23,14 @@ Rails.application.routes.draw do
     get    'users/edit' => 'users#edit'
     delete 'users/:id' => 'users#destroy'
   end
-
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
  
-  #get 'scrip/sc' => 'scrip#sc'
   resources :users do
     member do
       get :following, :followers
     end
   end
+
+  #get 'scrip/sc' => 'scrip#sc'
 
 end
 
