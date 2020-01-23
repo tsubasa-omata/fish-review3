61a7f42328e8db56a458134bf52c765deac22b97
小俣翼 <[runrunrun108@gmail.com]>
2020-01-23 22:28:22 +0900

    rspec編集

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'SignUp', type: :system do
-    before do
-      ActionMailer::Base.deliveries.clear
-    end
+#RSpec.describe 'SignUp', type: :system do
+#    before do
+#      ActionMailer::Base.deliveries.clear
+#    end
