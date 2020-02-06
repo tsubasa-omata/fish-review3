b757dfde55d91c2d15e9f3bf526df200ba7b051c
小俣翼 <[runrunrun108@gmail.com]>
2020-02-06 19:53:06 +0900

    header現在位置表示実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,3 +10,7 @@ module ApplicationHelper
     end
   end
 end
+
+def active?(controller_name , action_name)
+  return "active" if controller_name == params[:controller] && action_name == params[:action]
+end
